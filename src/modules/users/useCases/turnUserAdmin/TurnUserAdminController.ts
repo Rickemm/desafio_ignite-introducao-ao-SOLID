079477c813160c33efe27fd5a5cdcbd4c7f9ec48
Henrique Monteiro Martins <henrique.monteiro.martins@hotmail.com>
2022-03-03 12:57:38 -0300

    Procurando soluções

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -5,8 +5,11 @@ import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
+  handle(request: Request, response: Response): Response {    
+    const { user_id } = request.params;
+    const user = this.turnUserAdminUseCase.execute({user_id});
+
+    return response.json(user);
   }
 }
 
